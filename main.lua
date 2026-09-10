604958952919ff974b2c33f1141fb224dbd162eb
shariliendor <cheddargarden383@gmail.com>
2026-09-10 14:14:46 -0700

    a few tiny setup things

---
+++ main.lua
@@ -0,0 +1,6 @@
+require "ui"
+
+while true do
+    gui.text(0, 0, Test)
+    emu.frameadvance()
+end
---
+++ ui.lua
@@ -0,0 +1,9 @@
+local paused = false
+local debug = false
+local perfect_luck = false
+local hide_ui = false
+local disable_game_input = false
+
+function Draw_ui()
+    
+end
